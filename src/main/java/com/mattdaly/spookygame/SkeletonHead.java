c5fc98f37948ffd00235df5c476b612d8e94b67a
Matthew Daly <matthewd673@gmail.com>
2020-10-28 18:24:40 -0400

    Fix weird head particles

--- src/main/java/com/mattdaly/spookygame/SkeletonHead.java
+++ src/main/java/com/mattdaly/spookygame/SkeletonHead.java
@@ -14,21 +14,24 @@ public class SkeletonHead extends Entity {
 
     public void update() {
 
-        for(Entity e : Main.entityManager.entityList) {
+        //check collisions if head off body
+        if(!lastBody.hasHead) {
+            for (Entity e : Main.entityManager.entityList) {
 
-            if(e.col == null)
-                continue;
+                if (e.col == null)
+                    continue;
 
-            if(Collider.detectCollision(col.x, col.y, col.w, col.h, e.col.x, e.col.y, e.col.w, e.col.h)) {
-                switch(e.eType) {
-                    case Grave: //hit a grave and explode
-                        ParticleSystem headParticles = new ParticleSystem(col.x + 4, col.y + 4, 200, new Color(239, 105, 47));
+                if (Collider.detectCollision(col.x, col.y, col.w, col.h, e.col.x, e.col.y, e.col.w, e.col.h)) {
+                    switch (e.eType) {
+                        case Grave: //hit a grave and explode
+                            ParticleSystem headParticles = new ParticleSystem(col.x + 4, col.y + 4, 200, new Color(239, 105, 47));
 
-                        lastBody.pickupHead(this);
-                        break;
+                            lastBody.pickupHead(this);
+                            break;
+                    }
                 }
-            }
 
+            }
         }
 
         super.update();
