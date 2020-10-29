b0dacea3a00e4ddf41255a6451b463b1ac38bcf0
jdapello <jdapello@outlook.com>
2020-10-28 20:09:27 -0400

    added collisions to top fence

--- src/main/java/com/mattdaly/spookygame/Level.java
+++ src/main/java/com/mattdaly/spookygame/Level.java
@@ -39,7 +39,7 @@ public class Level {
             e.printStackTrace();
         }
 
-//hi
+//hi yep
     }
 
 
--- src/main/java/com/mattdaly/spookygame/TopFence.java
+++ src/main/java/com/mattdaly/spookygame/TopFence.java
@@ -4,6 +4,8 @@ public class TopFence extends Entity{
 
     public TopFence(int x, int y){
         super(EntityType.TopFence, Sprites.topFence, new Vector2(x, y), 32, 32);
+
+        col = new Collider(0, 23, 32, 6, false);
     }
 
 }
